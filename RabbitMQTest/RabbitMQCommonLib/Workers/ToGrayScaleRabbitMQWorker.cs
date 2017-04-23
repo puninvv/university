32b8bd73806bd07b518ae79a69e6505f55c6a691
Punin Viktor <punin.v.v@gmail.com>
2017-04-23 21:44:39 +0300

    added to grayscaleconverter

--- RabbitMQTest/RabbitMQCommonLib/Workers/ToGrayScaleRabbitMQWorker.cs
+++ RabbitMQTest/RabbitMQCommonLib/Workers/ToGrayScaleRabbitMQWorker.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RabbitMQCommonLib.Workers;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace RabbitMQCommonLib.Workers.ImageRabbitMQWorker
 {
@@ -11,7 +13,48 @@ namespace RabbitMQCommonLib.Workers.ImageRabbitMQWorker
     {
         public byte[] ProcessTask(RabbitMQTask _task)
         {
-            return _task.Data;
+            var serializer = new BytesSerializer<Bitmap>();
+
+            var bitmap = serializer.ByteArrayToObject(_task.Data);
+
+            var result = MakeGrayscale3(bitmap);
+
+            return serializer.ObjectToByteArray(result);
+        }
+
+        private static Bitmap MakeGrayscale3(Bitmap original)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][]
+               {
+                 new float[] {.3f, .3f, .3f, 0, 0},
+                 new float[] {.59f, .59f, .59f, 0, 0},
+                 new float[] {.11f, .11f, .11f, 0, 0},
+                 new float[] {0, 0, 0, 1, 0},
+                 new float[] {0, 0, 0, 0, 1}
+               });
+
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+               0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+
+            //dispose the Graphics object
+            g.Dispose();
+            return newBitmap;
         }
     }
 }
