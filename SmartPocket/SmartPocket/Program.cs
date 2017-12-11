5c4b7ee51140053dc7fecadec6d5bfba2c5f9712
Punin Viktor <punin.v.v@gmail.com>
2017-12-11 23:34:30 +0300

    + telegram.bot.

--- SmartPocket/SmartPocket/Program.cs
+++ SmartPocket/SmartPocket/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using log4net.Config;
 using SmartPocket.DALC;
 using System;
+using System.Threading;
 
 namespace SmartPocket
 {
@@ -11,6 +12,13 @@ namespace SmartPocket
     {
         public static void Main(string[] _args)
         {
+            var bot = new Telegram.Bot.TelegramBotClient("403796151:AAF7ia5i-jbet0jEFE2DltS9w263_SqCptk");
+            bot.OnMessage += Bot_OnMessage;
+        }
+
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            Logger.Log.Warn($"nameof(Bot_OnMessage):{e.Message.Text}");
         }
     }
 }
