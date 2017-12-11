4a005624325dad68064ad9d0b101b34aa43a11e8
Punin Viktor <punin.v.v@gmail.com>
2017-12-11 23:56:08 +0300

    подправил

--- SmartPocket/SmartPocket/Program.cs
+++ SmartPocket/SmartPocket/Program.cs
@@ -8,17 +8,35 @@ using System.Threading;
 
 namespace SmartPocket
 {
-    class Program
+    public class Program
     {
         public static void Main(string[] _args)
         {
+            Logger.Log.Info("Started");
             var bot = new Telegram.Bot.TelegramBotClient("403796151:AAF7ia5i-jbet0jEFE2DltS9w263_SqCptk");
             bot.OnMessage += Bot_OnMessage;
+            bot.StartReceiving();
+
+            Logger.Log.Info("Bot is working!");
+            Console.ReadKey();
+
+            bot.OnMessage -= Bot_OnMessage;
+            bot.StopReceiving();
+            Logger.Log.Info("Stopped");
         }
 
         private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
-            Logger.Log.Warn($"nameof(Bot_OnMessage):{e.Message.Text}");
+            Logger.Log.Info("New message!");
+            Logger.Log.Info($"{nameof(e.Message.Chat.FirstName)}:{e.Message?.Chat?.FirstName}");
+            Logger.Log.Info($"{nameof(e.Message.Chat.Id)}:{e.Message?.Chat?.Id}");
+            Logger.Log.Info($"{nameof(e.Message.Chat.Username)}:{e.Message?.Chat?.Username}");
+            Logger.Log.Info($"{nameof(e.Message.Contact.FirstName)}:{e.Message?.Contact?.FirstName}");
+            Logger.Log.Info($"{nameof(e.Message.Contact.LastName)}:{e.Message?.Contact?.LastName}");
+            Logger.Log.Info($"{nameof(e.Message.Contact.PhoneNumber)}:{e.Message?.Contact?.PhoneNumber}");
+            Logger.Log.Info($"{nameof(e.Message.Contact.UserId)}:{e.Message?.Contact?.UserId}");
+            Logger.Log.Info($"{nameof(e.Message.Text)}:{e.Message?.Text}");
+            Logger.Log.Info($"{nameof(e.Message)}:{e.Message}");
         }
     }
 }
